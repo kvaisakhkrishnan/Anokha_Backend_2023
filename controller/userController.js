2838897b47e3cfa832b908ed9353ffa8c102a84a
kvaisakhkrishnan <kvaisakhkrishnan@gmail.com>
2023-04-07 15:20:20 +0530

    Starred events updated

--- controller/userController.js
+++ controller/userController.js
@@ -506,8 +506,8 @@ module.exports = {
 
             let db_connection = await db.promise().getConnection();
             try{
-                await db_connection.query("lock tables starredevents read");
-                const [result] = await db_connection.query(`select * from starredevents where userEmail = ?`,[req.body.userEmail]);
+                await db_connection.query("lock tables AnokhaStarredEventsData read, eventData read, department read");
+                const [result] = await db_connection.query(`select * from AnokhaStarredEventsData where userEmail = ?`,[req.body.userEmail]);
                 await db_connection.query("unlock tables");
                 res.send(result);
             }
