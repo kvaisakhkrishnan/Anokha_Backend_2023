ddeba674d6e9c46060e32e9702ca3f0bfc9a021e
sharath sr <76243046+Sharath-a26@users.noreply.github.com>
2023-04-08 10:54:26 +0530

    svf

--- routes/adminWeb.js
+++ routes/adminWeb.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 const adminWebController = require('../controller/adminWebController');
 
-
+router.post("/login", adminWebController.adminLogin);
 module.exports = router;
+
