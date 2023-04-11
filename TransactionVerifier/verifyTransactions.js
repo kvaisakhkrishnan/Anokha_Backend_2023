5633dfbf8e190c3f611251248fbce5903a2bee67
kvaisakhkrishnan <kvaisakhkrishnan@gmail.com>
2023-04-11 13:13:25 +0530

    Updated

--- TransactionVerifier/verifyTransactions.js
+++ TransactionVerifier/verifyTransactions.js
@@ -63,79 +63,79 @@ const checkPaymentStatus = async () => {
                         
                         await db_connection.query('unlock tables');
                         
-                        // if(result.affectedRows == 0)
-                        // {
-                        //     await db_connection.query('lock tables transactions write');
-                        //     const [result] = await db_connection.query('update transactions set transactionStatus = "MALPERROR" where transactionId = ?', [response.data.transaction_details[individualTransaction].txnid]);
-                        //     await db_connection.query('unlock tables');
-                        // }
-                        // else{
-                        //     await db_connection.query('lock tables transactions read');
-                        //     const [output] = await db_connection.query('select * from transactions where transactionId = ?', [response.data.transaction_details[individualTransaction].txnid])
-                        //     await db_connection.query('unlock tables');
-                        //     if(output[0].productId == "P")
-                        //     {
-                        //         const conn = await db.promise().getConnection();
-                        //         try{
-                        //             await conn.query('lock tables userData write');
-                        //             const [passport] = await conn.query('select * from userData where userEmail = ?', [output[0].userEmail]);
-                        //             var passportId = "";
-                        //             if(passport[0].isAmritaCBE == 1)
-                        //             {
-                        //                 passportId = "A23A" + CRC32.str([output[0].userEmail]);
-                        //             }
-                        //             else{
-                        //                 passportId = "A23E" + CRC32.str([output[0].userEmail]);
-                        //             }
+                        if(result.affectedRows == 0)
+                        {
+                            await db_connection.query('lock tables transactions write');
+                            const [result] = await db_connection.query('update transactions set transactionStatus = "MALPERROR" where transactionId = ?', [response.data.transaction_details[individualTransaction].txnid]);
+                            await db_connection.query('unlock tables');
+                        }
+                        else{
+                            await db_connection.query('lock tables transactions read');
+                            const [output] = await db_connection.query('select * from transactions where transactionId = ?', [response.data.transaction_details[individualTransaction].txnid])
+                            await db_connection.query('unlock tables');
+                            // if(output[0].productId == "P")
+                            // {
+                            //     const conn = await db.promise().getConnection();
+                            //     try{
+                            //         await conn.query('lock tables userData write');
+                            //         const [passport] = await conn.query('select * from userData where userEmail = ?', [output[0].userEmail]);
+                            //         var passportId = "";
+                            //         if(passport[0].isAmritaCBE == 1)
+                            //         {
+                            //             passportId = "A23A" + CRC32.str([output[0].userEmail]);
+                            //         }
+                            //         else{
+                            //             passportId = "A23E" + CRC32.str([output[0].userEmail]);
+                            //         }
 
-                        //             const [fin] = await conn.query("update userData set passportId = ?, activePasspor = ? where userEmail = ?", [passportId, 1, [output[0].userEmail]]);
-                        //             await conn.query('unlock tables');
-                        //         }
-                        //         catch(err) {
-                        //             console.log(err);
-                        //             const now = new Date();
-                        //             now.setUTCHours(now.getUTCHours() + 5);
-                        //             now.setUTCMinutes(now.getUTCMinutes() + 30);
-                        //             const istTime = now.toISOString().slice(0, 19).replace('T', ' ');
-                        //             fs.appendFile('ErrorLogs/errorLogs.txt', istTime+"\n", (err)=>{});
-                        //             fs.appendFile('ErrorLogs/errorLogs.txt', err.toString()+"\n\n", (err)=>{});
-                        //             console.log("Contact DB Admin if you see this message");
-                        //         }
+                            //         const [fin] = await conn.query("update userData set passportId = ?, activePasspor = ? where userEmail = ?", [passportId, 1, [output[0].userEmail]]);
+                            //         await conn.query('unlock tables');
+                            //     }
+                            //     catch(err) {
+                            //         console.log(err);
+                            //         const now = new Date();
+                            //         now.setUTCHours(now.getUTCHours() + 5);
+                            //         now.setUTCMinutes(now.getUTCMinutes() + 30);
+                            //         const istTime = now.toISOString().slice(0, 19).replace('T', ' ');
+                            //         fs.appendFile('ErrorLogs/errorLogs.txt', istTime+"\n", (err)=>{});
+                            //         fs.appendFile('ErrorLogs/errorLogs.txt', err.toString()+"\n\n", (err)=>{});
+                            //         console.log("Contact DB Admin if you see this message");
+                            //     }
                     
-                        //         finally {
-                        //             await conn.release()
-                        //         }
-                        //     }
-                        //     else{
-                        //         var eventId = output[0].productId.substring(1);
-                        //         const conn = await db.promise().getConnection();
-                        //         try{
-                        //             const now = new Date();
-                        //             now.setUTCHours(now.getUTCHours() + 5);
-                        //             now.setUTCMinutes(now.getUTCMinutes() + 30);
-                        //             const istTime = now.toISOString().slice(0, 19).replace('T', ' ');
-                        //             await conn.query('lock tables registeredEvents write');
-                        //             const [out] = conn.query('insert into registeredEvents (userEmail, eventId, timeStamp, refundRequested) values (?,?,?,?)', [output[0].userEmail, eventId, istTime, 0])
-                        //             await conn.query('unlock tables');
-                        //             console.log(out);
-                        //         }
-                        //         catch(err) {
-                        //             console.log(err);
-                        //             const now = new Date();
-                        //             now.setUTCHours(now.getUTCHours() + 5);
-                        //             now.setUTCMinutes(now.getUTCMinutes() + 30);
-                        //             const istTime = now.toISOString().slice(0, 19).replace('T', ' ');
-                        //             fs.appendFile('ErrorLogs/errorLogs.txt', istTime+"\n", (err)=>{});
-                        //             fs.appendFile('ErrorLogs/errorLogs.txt', err.toString()+"\n\n", (err)=>{});
-                        //             console.log("Contact DB Admin if you see this message");
-                        //         }
+                            //     finally {
+                            //         await conn.release()
+                            //     }
+                            // }
+                            if(true){
+                                var eventId = output[0].productId.substring(1);
+                                const conn = await db.promise().getConnection();
+                                try{
+                                    const now = new Date();
+                                    now.setUTCHours(now.getUTCHours() + 5);
+                                    now.setUTCMinutes(now.getUTCMinutes() + 30);
+                                    const istTime = now.toISOString().slice(0, 19).replace('T', ' ');
+                                    await conn.query('lock tables registeredEvents write');
+                                    const [out] = conn.query('insert into registeredEvents (userEmail, eventId, timeStamp, refundRequested) values (?,?,?,?)', [output[0].userEmail, eventId, istTime, 0])
+                                    await conn.query('unlock tables');
+                                    console.log(out);
+                                }
+                                catch(err) {
+                                    console.log(err);
+                                    const now = new Date();
+                                    now.setUTCHours(now.getUTCHours() + 5);
+                                    now.setUTCMinutes(now.getUTCMinutes() + 30);
+                                    const istTime = now.toISOString().slice(0, 19).replace('T', ' ');
+                                    fs.appendFile('ErrorLogs/errorLogs.txt', istTime+"\n", (err)=>{});
+                                    fs.appendFile('ErrorLogs/errorLogs.txt', err.toString()+"\n\n", (err)=>{});
+                                    console.log("Contact DB Admin if you see this message");
+                                }
                     
-                        //         finally {
-                        //             await conn.release()
-                        //         }
+                                finally {
+                                    await conn.release()
+                                }
 
-                        //     }
-                        // }
+                            }
+                        }
                     }
                     catch(err) {
                         console.log(err);
