e2b720fe2b611700b8b189f46cbf9b55f949fc62
kvaisakhkrishnan <kvaisakhkrishnan@gmail.com>
2023-04-12 10:29:30 +0530

    Updated

--- controller/adminController.js
+++ controller/adminController.js
@@ -178,7 +178,7 @@ const { param } = require('../routes/userApp');
             }
             else if(req.body.authorization_tier == "DEPTHEAD")
             {
-                sql_q = `select * from EventData where departmentAbbr = (select * from eventManager where userName = ?)`;
+                sql_q = `select * from EventData where departmentAbbr = (select departmentAbbr from eventManager where userName = ?)`;
                 parameters = [req.body.userName]
             }
             else{
